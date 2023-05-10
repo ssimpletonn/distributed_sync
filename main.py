65d2b5cc8b2003b6ffc3cfdb989c97cfbf7b212f
Grigorii <pangr33n@gmail.com>
2023-05-10 09:48:27 +0000

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import types
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -12,6 +13,7 @@ import pandas
 from PIL import Image
 from torch.utils.data import DataLoader, random_split
 from torch.utils.data import Dataset
+import pickle
 
 num_workers = 3
 ray.init(address='auto',runtime_env={'env_vars': {'__MODIN_AUTOIMPORT_PANDAS__': '1'}})
@@ -57,12 +59,7 @@ df = pd.read_pickle("imagenet.pkl")
 
 #df.to_pickle("imagenet.pkl")
 
-tdf = pd.read_pickle("test_imagenet.pkl")
-
-print(df.head())
-print('df saved')
-
-
+tdf = pd.read_pickle("test_imagenet.pkl") 
 
 class CustomDataset(Dataset):
     def __init__(self, dataframe):
@@ -78,12 +75,12 @@ class CustomDataset(Dataset):
         path = self.dataframe.loc[index, 'path']
         label = self.dataframe.loc[index, 'label']
         image = Image.open(path).convert('RGB')
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(torch.Tensor(self.mean), torch.Tensor(self.std))
-            ])
+        transform = torchvision.models.EfficientNet_B0_Weights.IMAGENET1K_V1.transforms()#transforms.Compose([
+            #transforms.Resize((224, 224)),
+            #transforms.RandomHorizontalFlip(),
+            #transforms.ToTensor(),
+            #transforms.Normalize(torch.Tensor(self.mean), torch.Tensor(self.std))
+            #])
         image = transform(image)
         return image, label
 
@@ -100,11 +97,11 @@ class TestDataset(Dataset):
         path = self.dataframe.loc[index, 'path']
         label = self.dataframe.loc[index, 'label']
         image = Image.open(path).convert('RGB')
-        transform = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize(torch.Tensor(self.mean), torch.Tensor(self.std))
-            ])
+        transform = torchvision.models.EfficientNet_B0_Weights.IMAGENET1K_V1.transforms()#transforms.Compose([
+            #transforms.Resize((224, 224)),
+            #transforms.ToTensor(),
+            #transforms.Normalize(torch.Tensor(self.mean), torch.Tensor(self.std))
+            #])
         image = transform(image)
         return image, label
 
@@ -125,16 +122,19 @@ train_ds = []
 for i in range(num_workers):
     train_ds.append(CustomDataset(train_df[i]))
 
-train_loader = []
-train_loader_ref = []
+
+def get_data_loader(train_ds, test_ds, num_workers):
+    with FileLock(os.path.expanduser('./data.lock')):
+        train_loader = []
+        for i in range(num_workers):
+            train_loader.append(DataLoader(train_ds[i],batch_size=64, shuffle=True))
+        test_loader = DataLoader(test_ds, batch_size=64)
+    return train_loader, test_loader
 
 
-for i in range(num_workers):
-    train_loader_ref.append(ray.put(train_loader.append(DataLoader(train_ds[i],batch_size=2048, shuffle=True))))
+test_ds = TestDataset(tdf)
 
-test_ds=TestDataset(tdf)
-
-test_loader=DataLoader(test_ds,batch_size=2048)
+train_loader, test_loader = get_data_loader(train_ds, test_ds, num_workers) #=DataLoader(test_ds,batch_size=2048)
 
 
 def evaluate(model, test_loader):
@@ -145,15 +145,13 @@ def evaluate(model, test_loader):
         for batch_idx, (data, target) in enumerate(test_loader):
             data, target = data, target
             outputs = model(data)
-            _, predicted = torch.max(outputs.data, 1)
+            _, pred = torch.max(outputs, 1)
             total += target.size(0)
-            correct += (predicted == target).sum().item()
-    return 100.0 * correct / total
-
-    
+            correct += (pred == target).sum().item()
+    return 100.0 * correct / total 
 
 def get_weights(self):
-    return {k: v.cpu() for k, v in self.state_dict().items()}
+    return {k: v for k, v in self.state_dict().items()}
 
 def set_weights(self, weights):
     self.load_state_dict(weights)
@@ -169,12 +167,18 @@ def set_gradients(self, gradients):
     for g, p in zip(gradients, self.parameters()):
         if g is not None:
             p.grad = torch.from_numpy(g)
+        else:
+            print("Something is wrong!")
 
 @ray.remote(resources={"ps" : 1})
 class ParameterServer(object):
-    def __init__(self, lr, model, momentum, weight_decay):
-        self.model = model
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+    def __init__(self, lr, momentum, weight_decay):
+        self.model = torchvision.models.efficientnet_b0(weights=None)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)#, momentum=momentum)
+        self.model.set_gradients = types.MethodType(set_gradients, self.model)
+        self.model.get_weights = types.MethodType(get_weights, self.model)
+        for params in self.model.parameters():
+            params.requires_grad = True
 
     def apply_gradients(self, *gradients):
         summed_gradients = [
@@ -182,61 +186,63 @@ class ParameterServer(object):
         ]
         self.model.train()
         self.optimizer.zero_grad()
-        self.model.set_gradients(self.model, summed_gradients)
+        self.model.set_gradients(summed_gradients)
         self.optimizer.step()
-        return self.model.get_weights(self.model)
+        return self.model.get_weights()
 
     def get_weights(self):
-        return self.model.get_weights(self.model)
-    
-
+        return self.model.get_weights()
+ 
 @ray.remote(resources={"worker" : 1})
 class DataWorker(object):
-    def __init__(self, model, data_loader):
+    def __init__(self, data_loader):
         self.model = torchvision.models.efficientnet_b0(weights=None) 
         self.train_loader = data_loader
         self.data_iterator = iter(self.train_loader)
-        self.model.set_weights = set_weights
-        self.model.get_gradients = get_gradients
+        self.model.set_weights = types.MethodType(set_weights, self.model)
+        self.model.get_gradients = types.MethodType(get_gradients, self.model)
+        for params in self.model.parameters():
+            params.requires_grad = True
 
     def compute_gradients(self, weights):
-        self.model.set_weights(self.model, weights)
-        model.train()
+        self.model.train()
+        self.model.set_weights(weights)
         try:
             data, target = next(self.data_iterator)
-        except:  # When the epoch ends, start a new epoch.
+        except:  
             self.data_iterator = iter(self.train_loader)
             data, target = next(self.data_iterator)
         self.model.zero_grad()
         output = self.model(data)
-        loss = F.cross_entropy(output, target)
-        print(loss)
-        loss.backward()
-        return self.model.get_gradients(self.model)
-
+        loss = nn.CrossEntropyLoss()
+        out = loss(output, target)
+        print(out)
+        out.backward()
+        nn.utils.clip_grad_norm_(model.parameters(), 1)
+        return self.model.get_gradients()
 
 iterations = 200
 model = torchvision.models.efficientnet_b0(weights=None)
 
-model.get_weights = get_weights
-model.set_weights = set_weights
-model.get_gradients = get_gradients
-model.set_gradients = set_gradients
+model.get_weights = types.MethodType(get_weights, model)
+model.set_weights = types.MethodType(set_weights, model)
+model.get_gradients = types.MethodType(get_gradients, model)
+model.set_gradients = types.MethodType(set_gradients, model)
 
-ps = ParameterServer.remote(lr=0.08, model=model, momentum=0.9, weight_decay=1e-5)
-workers = [DataWorker.remote(model, train_loader_ref[i]) for i in range(num_workers)]
+for params in model.parameters():
+    params.requires_grad = True
+
+ps = ParameterServer.remote(lr=0.01, momentum=0.9, weight_decay=1e-5)
+workers = [DataWorker.remote(train_loader[i]) for i in range(num_workers)]
 
 print("Running synchronous parameter server training.")
 current_weights = ps.get_weights.remote()
 for i in range(iterations):
     gradients = [worker.compute_gradients.remote(current_weights) for worker in workers]
-    # Calculate update after all gradients are available.
     current_weights = ps.apply_gradients.remote(*gradients)
-    
-    # Evaluate the current model.
-    model.set_weights(model, ray.get(current_weights))
-    with open("model.pkl", "wb") as d:
-        pickle.dump(model, f)
+    model.set_weights(ray.get(current_weights))
+    with open(f'{i}_model.pkl', "wb") as d:
+        pickle.dump(model, d)
     accuracy = evaluate(model, test_loader)
     print("Iter {}: \taccuracy is {}".format(i + 1, accuracy))
 
